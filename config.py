a0c53afcab736668b894d69aecd43ce866c3eb2f
AMITETHECODER <81475681+AMITETHECODER@users.noreply.github.com>
2022-12-03 16:34:54 +0545

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "24348166")) #optional
+API_HASH = getenv("API_HASH", "bff912d8c5af31ed1c905b9d6527812c") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5222353449").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5222353449"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://amit69:amitop@cluster0.rzq2lfz.mongodb.net/?retryWrites=true&w=majority") )
+BOT_TOKEN = getenv("BOT_TOKEN", "5718523447:AAEH6upo2cdstu5i0-Kuwq_RCppnP8g3pqQ")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/1852830a7a290ce77f01c.jpg" )
+ALIVE_TEXT = getenv("ALIVE_TEXT", "kya huwa bro!! " )
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAqd9ZMCyDakSJinr6Kac_aqxnnX_oYOd9xd7V4INlf-twU4CkB3EAQKwKEh8uBQaSwTdvMQH4nxC3d0uulzcyr80RjcVpXuKKCmNHMkk6zYNjRD0S8Dd0cSxnrVdwZNsSHqSWbiSo_Gzo855uzXE2g6rsqkiddKh4JGxIC_kClVdQbap1Rrda2p7BwsIFpH9SBPF3Qb0BW4N8fpQPiCzlDREvUzpPwZCRVGacqMmOMrUaKhTzyJUkHN0vCFAHjgEp8PZuObbV5xFSHxmj7GaJxkRjQsAihbVQa5D7I6JZbqXXh5vzG3oz2qyn7pYppnBqH1W62lVs8Hhk4kgLR5qRnQAAAAFMS4sJAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
